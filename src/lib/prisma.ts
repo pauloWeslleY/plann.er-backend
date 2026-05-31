b77a4340349a48a7be5e72ff1bbb796d5d728cd8
pauloWeslleY <weslleyspfc13@outlook.com>
2026-05-31 13:03:15 -0300

    chore: update dependencies and improve database adapter
    - Updated devDependencies and dependencies in package.json for better compatibility and features.
    - Changed database adapter to better-sqlite3 in prisma.ts for improved performance.
    - Removed DATABASE_URL from schema.prisma as it's no longer needed.
    - Enhanced server.ts with Swagger documentation and health check endpoint.
    - Refactored server setup for better organization and clarity.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "@prisma/client";
+
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL! });
 
 export const prisma = new PrismaClient({
-  log: ['query'],
-})
+  adapter,
+  log: ["query"],
+});
--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,65 @@
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import { createTrip } from './routes/create-trip'
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { createTrip } from "./routes/create-trip";
+import { fastifySwagger } from "@fastify/swagger";
+import scalarAPIReference from "@scalar/fastify-api-reference";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
-} from 'fastify-type-provider-zod'
-import { confirmTrip } from './routes/confirm-trip'
-import { confirmParticipants } from './routes/confirm-participant'
-import { createActivity } from './routes/create-activity'
-import { getActivities } from './routes/get-activities'
-import { createLink } from './routes/create-link'
-import { getLinks } from './routes/get-links'
-import { getParticipants } from './routes/get-participants'
-import { createInvite } from './routes/create-invite'
-import { updateTrip } from './routes/update-trip'
-import { getTripDetails } from './routes/get-trip-details'
-import { getParticipant } from './routes/get-participant'
-import { errorHandler } from './error-handler'
-import { env } from './env'
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
+import { errorHandler } from "./error-handler";
+import { env } from "./env";
 
-const app = fastify()
+const app = fastify();
 
 app.register(cors, {
-  origin: '*',
-})
+  origin: "*",
+});
 
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "System API - Stock Manager",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
 
-app.setErrorHandler(errorHandler)
+app.register(scalarAPIReference, { routePrefix: "/docs" });
 
-app.register(createTrip)
-app.register(confirmTrip)
-app.register(confirmParticipants)
-app.register(createActivity)
-app.register(getActivities)
-app.register(createLink)
-app.register(getLinks)
-app.register(getParticipants)
-app.register(createInvite)
-app.register(updateTrip)
-app.register(getTripDetails)
-app.register(getParticipant)
+app.get("/health", async () => "OK");
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler(errorHandler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipants);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
+app.register(updateTrip);
+app.register(getTripDetails);
+app.register(getParticipant);
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log('Server running!')
-})
+  console.log("Server running!");
+});
